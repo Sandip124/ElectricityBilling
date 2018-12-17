750822d46841a2bce497f952311f4ad9696cd330
Sndp124 <chaudharysandip124@gmail.com>
2018-12-17 12:57:42 +0545

    Customer Class created

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+namespace ElectricityBilling.Models{
+
+    public class Customer{
+        public int customer_id{get; set;}
+        public string sc_no { get; set; }
+        public string customer_name { get; set; }
+        public string customer_address { get; set; }
+        public string customer_contact { get; set; }
+        public string counter_name { get; set; }
+        public string meter_no { get; set; }
+        public string phase { get; set; }
+        public int load { get; set; }
+        public float due_amount{get; set;}
+        public double initial_meter_reading { get; set; }
+    }
+}
